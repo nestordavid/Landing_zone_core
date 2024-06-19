262b3f5b6dd9a522776bf04b5e30ed9e898d5a9d
David Valero <david.valero@mckesson.com>
2024-06-19 09:22:31 -0500

    :construction: New PoC for Landing Zone

--- azcaf-enterprise-scale/1_lz/main.tf
+++ azcaf-enterprise-scale/1_lz/main.tf
@@ -17,12 +17,12 @@ provider "azurerm" {
 module "lz-vending" {
     for_each = var.lz_vending
   source   = "Azure/lz-vending/azurerm"
-  version  = "3.4.1"
+  version  = "4.1.1"
   location = "centralus"
 
   subscription_alias_enabled = true
   subscription_alias_name    = each.value.subscription_alias_name
-  subscription_billing_scope = ""
+  subscription_billing_scope = "3ae5db7a-9d4b-5abb-20bd-1af2ecf39a1e:31806f85-8d2d-4e59-b04d-9844eaf7e670_2019-05-31"
   subscription_display_name  = each.value.subscription_display_name
   subscription_workload      = each.value.subscription_workload
 }
---
+++ azcaf-enterprise-scale/1_lz/subscription.tfvars
@@ -0,0 +1,14 @@
+lz_vending = {
+    subscription01 = {
+        subscription_alias_enabled = true
+        subscription_alias_name = "demo-production"
+        subscription_display_name = "demo visier"
+        subscription_worload = "Production"    
+    }
+    subscription02 = {
+        subscription_alias_enabled = true
+        subscription_alias_name = "demo-development"
+        subscription_display_name = "demo visier"
+        subscription_worload = "Development"    
+    }
+}
